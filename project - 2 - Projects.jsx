f6768f6100268e31e10f4c41484330efc3bed895
boddapraneetha <132537352+boddapraneetha@users.noreply.github.com>
2025-06-23 15:13:16 +0530

    Create project - 2 - Projects.jsx

---
+++ project - 2 - Projects.jsx
@@ -0,0 +1,9 @@
+function Projects() {
+  return (
+    <section>
+      <h2>Projects</h2>
+      <p>Beginner to advanced level categorized projects</p>
+    </section>
+  );
+}
+export default Projects;
