bf9444dedbf22e5ee4b09c777b85169350b31044
boddapraneetha <132537352+boddapraneetha@users.noreply.github.com>
2025-06-23 15:12:19 +0530

    Create project - 2- Home.jsx

---
+++ project - 2- Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <section>
+      <h1>Welcome to Edu Platform</h1>
+      <p>Learn programming through tutorials and projects</p>
+    </section>
+  );
+}
+export default Home;
