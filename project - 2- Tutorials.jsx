f0c87b398a7c5d1f28f21285dfd7e59c824b5151
boddapraneetha <132537352+boddapraneetha@users.noreply.github.com>
2025-06-23 15:12:47 +0530

    Create project - 2- Tutorials.jsx

---
+++ project - 2- Tutorials.jsx
@@ -0,0 +1,9 @@
+function Tutorials() {
+  return (
+    <section>
+      <h2>Tutorials</h2>
+      <p>List of tutorials categorized by language (Java, Python...)</p>
+    </section>
+  );
+}
+export default Tutorials;
